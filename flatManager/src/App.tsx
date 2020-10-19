50224a9b93daaa8a0254870b39faa54f5d6468e4
Olivier <olivier.verrier@ynov.com>
2020-10-19 11:46:55 +0200

    Revert "ajout modal, avec soucis"
    This reverts commit 090e55cc0ea16a45ad47ecbd27a18e9166c40522.

--- flatManager/src/App.tsx
+++ flatManager/src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-// import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonButton, IonModal, IonRouterOutlet } from '@ionic/react';
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -21,31 +22,16 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import Modal from './components/Modal';
 
-const App: React.FC = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  function closeModal() {
-    setShowModal(false);
-  }
-
-  return (
+const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
+        <Route path="/home" component={Home} exact={true} />
+        <Route exact path="/" render={() => <Redirect to="/home" />} />
       </IonRouterOutlet>
     </IonReactRouter>
-
-    <IonModal isOpen={showModal}>
-      <Modal 
-        showModal={showModal}
-        setShowModal={setShowModal}
-        closeAction={closeModal}>
-      </Modal>
-    </IonModal>
-    <IonButton onClick={() => setShowModal(true)}>Show Modal</IonButton>
   </IonApp>
-)};
+);
 
 export default App;
